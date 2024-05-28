603df13f8e56b49d783fe05f0c6137dbe1909cfc
John Riggles <jriggles@icloud.com>
2024-05-28 18:22:25 -0400

    v1.0.0 release
    update OS detection (requires Aseprite 1.3.8, API v28)
    
    templats are now sorted by width
    
    UI updates

--- extension/canvas-templates.lua
+++ extension/canvas-templates.lua
@@ -19,175 +19,199 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
 local preferences = {}
 local templatesFilePath = app.fs.userConfigPath .. "extensions/canvas-templates/templates.json"
 
-local function getOS()
-    return package.config:sub(1, 1) == "\\" and "Windows" or "Unix"
-end
-
 -- load canvas size templates from templates.json file
 local function loadTemplates()
-    local file = assert(io.open(templatesFilePath, "r+"), "Could not load templates file")
-    local data = file:read("*a")
-    local templates = json.decode(data)
-    -- FIXME: fix table ordering so dialog matches templates.json
-    file:close()
-    return templates
+  local file = assert(io.open(templatesFilePath, "r+"), "Could not load templates file")
+  local data = file:read("*a")
+  local templates = json.decode(data)
+  file:close()
+  return templates
 end
 
 local function fileExists(path)
-    local f = io.open(path, "r")
-    return f ~= nil and io.close(f)
+  local f = io.open(path, "r")
+  return f ~= nil and io.close(f)
 end
 
 local function openTemplatesJSON()
-    -- use os.execute to open the templates.json file in the default external editor
-    if not fileExists(templatesFilePath) then
-        app.alert{
-            title = "Template File Not Found",
-            text = "The file \"" .. templatesFilePath .. "\" does not exist."
-        }
-    end
-    if getOS() == "Unix" then
-        os.execute("open \"" .. templatesFilePath .. '"')
-    elseif getOS() == "Windows" then
-        os.execute("start \"" .. '"' .. templatesFilePath .. '"')
-    end
+  -- use os.execute to open the templates.json file in the default external editor
+  if not fileExists(templatesFilePath) then
+    app.alert{
+      title = "Template File Not Found",
+      text = "The file \"" .. templatesFilePath .. "\" does not exist."
+    }
+  end
+  if app.os.macos then
+    os.execute("open \"" .. templatesFilePath .. '"')
+  elseif app.os.windows then
+    os.execute("start \"" .. '"' .. templatesFilePath .. '"')
+  end
 end
 
 local function toggleCategory(dlg, toggleId, templates)
-    -- get the value of the toggle (checkbox) that triggered this callback
-    local visibility = dlg.data[toggleId]
-    -- hide the separator so there aren't a bunch left on the dlg when categories are hidden
-    dlg:modify { id = toggleId .. "Separator", visible = visibility }
-    -- iterate over the templates data to show/hide all items in the appropriate category
-    for category, templateNames in pairs(templates) do
-        for templateName, _size in pairs(templateNames) do
-            if toggleId == category then
-                dlg:modify { id = templateName, visible = visibility }
-            end
+  -- get the value of the toggle (checkbox) that triggered this callback
+  local visibility = dlg.data[toggleId]
+  -- hide the separator so there aren't a bunch left on the dlg when categories are hidden
+  dlg:modify { id = toggleId .. "Separator", visible = visibility }
+  -- iterate over the templates data to show/hide all items in the appropriate category
+  for category, templateNames in pairs(templates) do
+    for templateName, _size in pairs(templateNames) do
+      if toggleId == category then
+        dlg:modify { id = templateName, visible = visibility }
+        if visibility then
+          dlg:modify { id = toggleId, label = "▼"}
+        else
+          dlg:modify { id = toggleId, label = "►"}
         end
+      end
     end
+  end
 end
 
 local function badJSONDlg()
-    local loadErrorDlg = Dialog("Error Reading Template File")
-        :label { text = "There is a problem with your templates.json file." }
-        :newrow()
-        :label { text = "Please ensure the contents of this file are properly formatted!" }
-        :newrow()
-        :label { text = "Attempted to read from:" }
-        :newrow()
-        :label { text = templatesFilePath }
-        :button { text = "OK", hexpand = false }
-        :button { text = 'Open "templates.json"...', onclick = openTemplatesJSON }
-        :show()
+  local loadErrorDlg = Dialog("Error Reading Template File")
+  :label { text = "There is a problem with your templates.json file." }
+  :newrow()
+  :label { text = "Please ensure the contents of this file are properly formatted!" }
+  :newrow()
+  :label { text = "Attempted to read from:" }
+  :newrow()
+  :label { text = templatesFilePath }
+  :button { text = "OK", hexpand = false }
+  :button { text = 'Open "templates.json"...', onclick = openTemplatesJSON }
+  :show()
+end
+
+local function sortResolutions(category)
+  local resolutions = {}
+  for name, dimensions in pairs(category) do
+    table.insert(
+      resolutions,
+      { name = name, width = dimensions.width, height = dimensions.height }
+    )
+  end
+
+  table.sort(
+    resolutions,
+    function(a, b)
+      return a.width < b.width
+    end
+  )
+  return resolutions
 end
 
 local function templateSelectionDialog(templates)
-    local dlg = Dialog("Canvas Size Templates")
-        -- show dimensions of currently selected template
-        dlg:label { id = "sizeHint", text = "Template Size: (choose a template)"}
+  local dlg = Dialog("Canvas Size Templates")
+  -- show dimensions of currently selected template
+  dlg:label { id = "sizeHint", text = "Template Size: (choose a template)"}
 
-    -- get template categories
-    for category, templateNames in pairs(templates) do
-        -- add labeled separator for each category
-        dlg:separator()
-        dlg:check {
-            id = category,
-            text = category .. ":",
-            selected = true, -- checked, categories should be visible by default
-            hexpand = false,
-            onclick = function() toggleCategory(dlg, category, templates) end,
-        }
-        -- this separator has an id so it can be hidden by toggleCategory as-needed
-        dlg:separator { id = category .. "Separator" }
-        local i = 1
-        -- get sizes in each category
-        for templateName, size in pairs(templateNames) do
-            -- get width and height without decimals
-            local wStatus, wInt = pcall(math.floor, size.width)
-            local hStatus = pcall(math.floor, size.height)
-            print(wStatus, wInt, hStatus, hInt)
-
-            if not wStatus and not hStatus then
-                badJSONDlg()
-                return
-            end
-            -- add radio button for each size
-            dlg:radio {
-                -- hexpand = false,
-                id = templateName,
-                text = templateName,
-                onclick = function ()
-                    dlg:modify {
-                        id = "sizeHint",
-                        text = "Template Size: " .. wInt .. "x" .. hInt
-                    }
-                end
-            }
-            -- insert a newrow after every 3 items
-            local itemsPerRow = 2
-            if i % itemsPerRow == 0 then
-                dlg:newrow()
-            end
-            i = i + 1
-        end
-    end
-
-    dlg:separator { text = "Edit Templates" }
-    dlg:button {
-        id = "openJson",
-        text = 'Open "templates.json"...',
-        onclick = function()
-            openTemplatesJSON()
-            dlg:close()
-        end
-    }
-    dlg:label { text = "(opens in default external editor)", align = Align.CENTER }
-
+  -- get template categories
+  for category, templateNames in pairs(templates) do
+    -- add labeled separator for each category
     dlg:separator()
-    dlg:button {
-        id = "select",
-        text = "Select",
-        onclick = function() -- apply selected template
-            for category, templateNames in pairs(templates) do
-                for templateName, size in pairs(templateNames) do
-                    if dlg.data[templateName] then
-                        -- create a new sprite with the given dimensions
-                        app.command.NewFile{ ui = false, width = size.width, height = size.height }
-                    end
-                end
-            end
-            dlg:close()
-        end
+    dlg:check {
+      align = Align.CENTER,
+      id = category,
+      text = category,
+      label = "▼",
+      selected = true, -- checked, categories should be visible by default
+      hexpand = true,
+      onclick = function() toggleCategory(dlg, category, templates) end,
     }
-    dlg:button { id = "cancel", text = "Cancel" }
-    dlg:show()
+    -- this separator has an id so it can be hidden by toggleCategory as-needed
+    dlg:separator { id = category .. "Separator" }
+    local i = 1
+    -- get sizes in each category
+    templateNames = sortResolutions(templateNames)
+    for templateName, data in pairs(templateNames) do
+      -- get width and height without decimals
+      local wStatus, wInt = pcall(math.floor, data.width)
+      local hStatus, hInt = pcall(math.floor, data.height)
+
+      if not wStatus and not hStatus then
+        badJSONDlg()
+        return
+      end
+      -- add radio button for each size
+      dlg:radio {
+        -- hexpand = false,
+        id = data.name,
+        text = data.name,
+        onclick = function ()
+          dlg:modify {
+            id = "sizeHint",
+            text = "Template Size: " .. wInt .. "x" .. hInt
+          }
+        end
+      }
+      -- insert a newrow after every 2 items
+      local itemsPerRow = 2
+      if i % itemsPerRow == 0 then
+        dlg:newrow()
+      end
+      i = i + 1
+    end
+  end
+
+  dlg:separator { text = "Edit Templates" }
+  dlg:button {
+    id = "openJson",
+    text = 'Open "templates.json"...',
+    onclick = function()
+      openTemplatesJSON()
+      dlg:close()
+    end
+  }
+  dlg:label { text = "(opens in default external editor)", align = Align.CENTER }
+
+  dlg:separator()
+  dlg:button {
+    id = "select",
+    text = "Select",
+    onclick = function() -- apply selected template
+      for category, templateNames in pairs(templates) do
+        for templateName, size in pairs(templateNames) do
+          if dlg.data[templateName] then
+            -- create a new sprite with the given dimensions
+            app.command.NewFile{ ui = false, width = size.width, height = size.height }
+          end
+        end
+      end
+      dlg:close()
+    end
+  }
+  dlg:button { id = "cancel", text = "Cancel" }
+  dlg:show()
 end
 
 local function main()
-    local status, templateData = pcall(loadTemplates)
-    if not status then
-        badJSONDlg()
-    end
-    templateSelectionDialog(templateData)
-    app.command.refresh()
+  if app.apiVersion < 28 then
+    return app.alert("This extension requires Aseprite version 1.3.7 or later")
+  end
+
+  local status, templateData = pcall(loadTemplates)
+  if not status then
+    badJSONDlg()
+  end
+  templateSelectionDialog(templateData)
+  app.command.refresh()
 end
 
 -- Aseprite plugin API stuff...
 ---@diagnostic disable-next-line: lowercase-global
 function init(plugin) -- initialize extension
-    preferences = plugin.preferences
+  preferences = plugin.preferences
 
-    plugin:newCommand {
-        id = "openTemplates",
-        title = "Canvas Templates...",
-        group = "file_recent",
-        onclick = main -- run main function
-    }
+  plugin:newCommand {
+    id = "openTemplates",
+    title = "Canvas Templates...",
+    group = "file_recent",
+    onclick = main -- run main function
+  }
 end
 
 ---@diagnostic disable-next-line: lowercase-global
 function exit(plugin)
-    -- no cleanup
-    return nil
+  -- no cleanup
+  return nil
 end
